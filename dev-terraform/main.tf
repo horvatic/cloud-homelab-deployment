a3f9415e324755caa3296beee3fe2dc0b84e3839
Shawn Horvatic <shorvatic@8thlight.com>
2022-03-25 16:31:36 -0500

    fix ec2 deploy errors

--- dev-terraform/main.tf
+++ dev-terraform/main.tf
@@ -2,15 +2,6 @@ provider "aws" {
   region = "${REGION}"
 }
 
-data "aws_vpc" "main" {
-  id = "${VPC_ID}"
-}
-
-data "aws_subnet" "subnet" {
-  vpc_id            = data.aws_vpc.main.id
-  availability_zone = "us-east-2a"
-}
-
 data "aws_ami" "ubuntu" {
   most_recent = true
 
@@ -27,21 +18,12 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-resource "aws_network_interface" "network_interface" {
-  subnet_id       = aws_subnet.subnet.id
-  security_groups = ["${SECURITY_GROUP}"]
-}
-
 resource "aws_instance" "dev" {
   ami                         = data.aws_ami.ubuntu.id
   instance_type               = "t3.large"
   associate_public_ip_address = true
   key_name                    = "${KEY_PAIR_NAME}"
-  network_interface {
-    network_interface_id   = aws_network_interface.network_interface.id
-    device_index           = 0
-    delete_on_termination = true
-  }
+
   credit_specification {
     cpu_credits = "standard"
   }
