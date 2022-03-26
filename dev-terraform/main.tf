9b2493b20800027425e0a3025e866efad3f3e6c3
Shawn Horvatic <100594894+sbhorvatic@users.noreply.github.com>
2022-03-26 17:39:47 -0500

    add dev machine to horvatic network (#13)

--- dev-terraform/main.tf
+++ dev-terraform/main.tf
@@ -18,11 +18,22 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
+data "aws_vpc" "main" {
+  id = "${VPC_ID}"
+}
+
+data "aws_subnet" "subnet" {
+  vpc_id            = data.aws_vpc.main.id
+  availability_zone = "us-east-2a"
+}
+
 resource "aws_instance" "dev" {
   ami                         = data.aws_ami.ubuntu.id
   instance_type               = "t3.large"
   associate_public_ip_address = true
   key_name                    = "${KEY_PAIR_NAME}"
+  security_groups = ["${SECURITY_GROUP}"]
+  subnet_id = data.aws_subnet.subnet.id
 
   credit_specification {
     cpu_credits = "standard"
